d22711dc1f45c40568e888ec925a1038476476aa
woyo <adisiswoyo@gmail.com>
2021-07-07 21:04:03 +0700

    add mail attributes

--- database/seeders/MailAttributeSeeder.php
+++ database/seeders/MailAttributeSeeder.php
@@ -15,31 +15,75 @@ class MailAttributeSeeder extends Seeder
     public function run()
     {
         MailAttribute::create([
-            "type" => 'Permohonan',
+            "type" => 'mail_type',
             'name' => 'PERMOHONAN',
             "code" => 'PRMHN',
             "color" => '#1bcfb4',
         ]);
 
         MailAttribute::create([
-            "type" => 'Pemberitahuan',
+            "type" => 'mail_type',
             'name' => 'PEBERITAHUAN',
             "code" => 'PBRTHN',
             "color" => '#ffd500',
         ]);
 
         MailAttribute::create([
-            "type" => 'Keterangan',
+            "type" => 'mail_type',
             'name' => 'KETERANGAN',
             "code" => 'KTRGN',
             "color" => '#1bcfb4',
         ]);
 
         MailAttribute::create([
-            "type" => 'Undanga',
+            "type" => 'mail_type',
             'name' => 'UNDANGAN',
             "code" => 'UNDGN',
             "color" => '#c3bdbd',
+        ]);
+
+        // Mail Reference
+        MailAttribute::create([
+            "type" => 'mail_reference',
+            'name' => 'Umum',
+            "code" => 'UM',
+            "color" => '#fe5678',
+        ]);
+
+        MailAttribute::create([
+            "type" => 'mail_reference',
+            'name' => 'Tertutup',
+            "code" => 'TRTP',
+            "color" => '#ffd500',
+        ]);
+
+        MailAttribute::create([
+            "type" => 'mail_reference',
+            'name' => 'Rahasia',
+            "code" => 'RHS',
+            "color" => '#1bcfb4',
+        ]);
+
+        // Mail Priority
+        MailAttribute::create([
+            "type" => 'mail_priority',
+            'name' => 'Biasa',
+            "code" => 'BS',
+            "color" => '#1bcfb4',
+        ]);
+
+        MailAttribute::create([
+            "type" => 'mail_priority',
+            'name' => 'Segera',
+            "code" => 'SGR',
+            "color" => '#ffd500',
+        ]);
+
+        MailAttribute::create([
+            "type" => 'mail_priority',
+            'name' => 'Mendesak',
+            "code" => 'MNDSK',
+            "color" => '#fe5678',
         ]);
     }
 }
