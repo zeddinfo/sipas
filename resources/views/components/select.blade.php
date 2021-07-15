92fbad1019209d46404a0c259d4d97fee8ed0410
woyo <adisiswoyo@gmail.com>
2021-07-15 22:45:53 +0700

    Form Edit

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -2,7 +2,7 @@
 <select class="form-control selectpicker show-tick" id="{{ $name }}" name="{{ $name }}"
         data-live-search="true" title="Pilih {{ $label }}">
     @forelse($options as $option)
-        <option value="{{ $option->id }}" selected={{isset($value) ? true : false}}>
+        <option value="{{ $option->id }}" {{$option->id == $value ? 'selected' : ''}}>
             {{ $option->name }}
         </option>
     @empty
